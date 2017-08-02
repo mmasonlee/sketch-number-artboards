b5ce48e49e237c047091e63b15d40a77493582a9
Mason Lee <mmason@fb.com>
2017-08-01 23:33:48 -0700

    Initial commit

---
+++ NumberArtboards.sketchplugin/Contents/Sketch/toptobottom.js
@@ -0,0 +1,33 @@
+var onRun = function(context) {
+    // reference the sketch document
+    var doc = context.document;
+    var selection = context.selection;
+    var numArtboard;
+
+    // check if user selected something
+    if (selection.count() == 0) {
+        doc.showMessage("Please select artboards.");
+    }
+
+    // check if every layer selected is an Artboard
+    var selectedArtboards = selection.every(function(layer){
+        return layer.isMemberOfClass(MSArtboardGroup)
+    });
+
+    if (!selectedArtboards) {
+        doc.showMessage("Only artboards allowed. Please only select artboards!.");
+    }
+
+    else {
+        numArtboard = selection.count();
+        // loop through the selected layers
+        for (var i = 0; i < numArtboard; i++) {
+            // reference the selection
+            var artboard = selection[i];
+            var artboardName = artboard.name();
+            var newArtboardName = numArtboard-i + "." + artboardName;
+            artboard.setName(newArtboardName);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# sketch-number-artboards
+
+## How does it work?
+1. Place artboards in order on layer list panel
+2. Select artboards
+3. Press `command shift .` or `command shift ,`
+
+![group 13](https://user-images.githubusercontent.com/16979294/28198768-66f223d6-6815-11e7-9aee-76ec1e86c775.png)
